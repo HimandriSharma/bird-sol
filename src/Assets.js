4a537e7a11d532cad0de292e8cb21b64fbab72f2
Himandri Sharma <himandrisharma27@gmail.com>
2022-03-17 16:38:10 +0530

    Initialized Properly

--- src/Assets.js
+++ src/Assets.js
@@ -1,12 +1,12 @@
-import dots from "./Assets/images/dots.svg";
-import counting from "./Assets/images/Counting.png";
-import UIDesign from "./Assets/images/UIDesignIllustration.svg";
-import Maintenance from "./Assets/images/MaintenanceIllustration.svg";
-import DevelopmentIllustration from "./Assets/images/DevelopmentIllustration.svg";
-import Expert from "./Assets/images/DiscussIllustration.svg";
-import Work from "./Assets/images/HowWeWorkIllustration.svg";
-import Onboarding from "./Assets/images/Onboarding.svg";
-import Neon from "./Assets/images/Neon.svg";
+import dots from './Assets/images/dots.svg'
+import counting from './Assets/images/Counting.png'
+import UIDesign from './Assets/images/UIDesignIllustration.svg'
+import Maintenance from './Assets/images/MaintenanceIllustration.svg'
+import DevelopmentIllustration from './Assets/images/DevelopmentIllustration.svg'
+import Expert from './Assets/images/DiscussIllustration.svg'
+import Work from './Assets/images/HowWeWorkIllustration.svg'
+import Onboarding from './Assets/images/Onboarding.svg'
+import Neon from './Assets/images/Neon.svg'
 
 export {
   dots,
@@ -18,4 +18,4 @@ export {
   Work,
   Onboarding,
   Neon,
-};
+}
